00bd42f3cf1ebd1076027d1e6fe6727f51f880b8
ipanbadru <ipanbzn1@gmail.com>
2022-02-07 09:26:31 +0700

    'wip

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function About() {
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome Portfolio <Link href={'/about'}><a>Here!</a></Link>
+          About Me
         </h1>
 
         <p className={styles.description}>
